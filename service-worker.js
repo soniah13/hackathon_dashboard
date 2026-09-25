1f26ab8b745caa76cb968577163983926ac3978d
sonia <imorgansonia@gmail.com>
2026-09-25 13:39:39 +0300

    refactor: update service worker cache version to v3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'hackathon-dashboard-v2';
+const CACHE_NAME = 'hackathon-dashboard-v3';
 const APP_SHELL = ['./', './index.html', './dist/output.css', './manifest.json'];
 
 self.addEventListener('install', event => {
